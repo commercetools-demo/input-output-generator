d61597f8b541239bffbf360052eb1563d5f605ac
Behnam Tehrani <behnam777@gmail.com>
2024-01-19 14:16:54 -0500

    ArrayNode updates labels

--- configurator-mc-app/src/components/editor/Nodes/ArrayNode.ts
+++ configurator-mc-app/src/components/editor/Nodes/ArrayNode.ts
@@ -11,22 +11,32 @@ export class ArrayNode extends ClassicPreset.Node<
   {
     item: ClassicPreset.Socket;
   },
-  {}
+  {
+    index: ClassicPreset.InputControl<'number'>;
+  }
 > {
+  path: string = '';
   height = INITIAL_HEIGHT;
   width = INITIAL_WIDTH;
   options?: EditorExtraOptions;
   returningObject: Record<string, any> = {};
+  index = 0;
   change?: () => void;
 
   constructor(options?: EditorExtraOptions, change?: () => void) {
     super('ARRAY');
+    this.change = change;
 
-    const arrayItem = new ClassicPreset.Input(socket, 'An array');
-    arrayItem.addControl(
-      new ClassicPreset.InputControl('text', { initial: '', change })
-    );
+    const arrayItem = new ClassicPreset.Input(socket, 'array');
     this.addInput('array', arrayItem);
+
+    this.addControl(
+      'index',
+      new ClassicPreset.InputControl('number', {
+        initial: 0,
+        change: this.updateIndex,
+      })
+    );
 
     const output = new ClassicPreset.Output(socket, 'Item');
     this.addOutput('item', output);
@@ -34,14 +44,42 @@ export class ArrayNode extends ClassicPreset.Node<
     this.options = options;
   }
 
+  updateIndex = (value: number) => {
+    if (value < 0) {
+      this.index = 0;
+    } else {
+      this.index = value;
+    }
+    this.change?.();
+    this.options?.area?.update('node', this.id);
+  };
+
   async data(inputs: { array: any[] }): any {
+    const connections = this.options?.editor?.getConnections();
+
+    const connectionToThisNode = connections?.find((connection) => {
+      return connection?.target === this.id;
+    });
+
+    if (connectionToThisNode && this.inputs.array) {
+      this.inputs.array.label = connectionToThisNode.sourceOutput;
+      this.path = this.inputs.array.label;
+    }
+
     const { array } = inputs;
 
     if (!array?.[0] || array?.[0]?.length === 0) {
-      return {};
+      return { item: {} };
     }
-    console.log(this.outputs.item);
+    if (this.outputs.item) {
+      this.outputs.item.label = `ID: ${
+        array[0][this.index]?.id
+          ? array[0][this.index]?.id.toString().substring(0, 6) + '...'
+          : ''
+      }`;
+    }
+    this.options?.area?.update('node', this.id);
 
-    return { item: array[0][0] };
+    return { item: array[0][this.index] };
   }
 }
